9a481aa2d2c93938f581d2658464437faac2ec61
ayan4m1 <andrew@bulletlogic.com>
2012-11-14 09:12:52 -0500

    initial import

--- rrd-create.php
+++ rrd-create.php
@@ -1,4 +1,21 @@
 <?php
+/*
+	This file is part of Blissmon.
+
+	Blissmon is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Blissmon is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Blissmon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 rrd_create('bliss.rrd', array(
 	'--step', '15',
 	'--start', '1351773830',
